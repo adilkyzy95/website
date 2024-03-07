23539e236c15af0fb2f3420edf72f8fb4d3229b8
adilkyzy95 <ulzhalgastolepbergen229@gmail.com>
2024-03-07 12:25:21 -0800

    Update and rename index.html to index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// script.js
+function showSection(sectionId) {
+    var sections = document.querySelectorAll('section');
+    sections.forEach(function(section) {
+        if (section.id === sectionId) {
+            section.style.display = 'block';
+        } else {
+            section.style.display = 'none';
+        }
+    });
+}
